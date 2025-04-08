95df21f25b1d64668c295c32f828fb92397b568e
Rade Guberinic <rade.guberinic@sqa-consulting.com>
2025-04-08 10:58:32 +0200

    final app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule  // Import FormsModule here
+    FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
