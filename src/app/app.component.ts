8d713c46da8f6f19cecd4cf5b120cb14d04a1f2f
Rade Guberinic <rade.guberinic@sqa-consulting.com>
2025-04-08 10:57:45 +0200

    inital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'todo-app-rade';
+export class AppComponent implements OnInit {
+  title = 'Rade Todo App';
+  todos: Todo[] = [];
+  newTodo: string = '';
+
+  ngOnInit() {
+    const savedTodos = localStorage.getItem('todos');
+    if (savedTodos) {
+      this.todos = JSON.parse(savedTodos);
+    }
+  }
+
+  addTodo() {
+    if (this.newTodo.trim()) {
+      const todo: Todo = {
+        id: Date.now(),
+        title: this.newTodo.trim(),
+        completed: false
+      };
+      this.todos.push(todo);
+      this.newTodo = '';
+      this.updateLocalStorage();
+    }
+  }
+
+  toggleTodo(todo: Todo) {
+    todo.completed = !todo.completed;
+    this.updateLocalStorage();
+  }
+
+  removeTodo(todo: Todo) {
+    this.todos = this.todos.filter(t => t.id !== todo.id);
+    this.updateLocalStorage();
+  }
+
+  updateLocalStorage() {
+    localStorage.setItem('todos', JSON.stringify(this.todos));
+  }
 }
